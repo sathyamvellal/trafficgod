6f014c22813b7643c7cbb092745c3ed343846c82
Sathyam Vellal <sathyam.vellal@gmail.com>
2015-11-26 00:32:59 +0530

    Using express; mongo for storage; views give plot

--- index.js
+++ index.js
@@ -1,22 +1,102 @@
-'use strict';
-
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var debug = require('debug')('trafficgod:server');
 var http = require('http');
+var trafficLogger = require('./lib/trafficLogger');
 
-var direction = process.env['DIRECTION'];
-var shouldRun = true;
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-switch (direction) {
-    case 'north':
-        require('./north').run();
-    break;
-    case 'south':
-        require('./south').run();
-    break;
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// set port, create server and listen
+var port = process.env.PORT || '8000';
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+
+server.on('error', function(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
     default:
-        console.log('Specify a direction!');
-        shouldRun = false;
-}
+      throw error;
+  }
+});
 
-if (shouldRun) {
-    http.createServer().listen(9999);
-}
+server.on('listening', function() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+});
+
+trafficLogger.start();
